f9678d6a4722dd9ab99ea08efc2600249711219c
Ashadowmonarch <forcoding39@gmail.com>
2025-07-01 10:01:15 -0400

    save point

--- xor.c
+++ xor.c
@@ -9,3 +9,23 @@ void printchar (char c, char *text)
 {
     printf("%s = '%c' (0x%x)\n", text, c, c);
 }
+
+int main()
+{
+    char cleartext, key, ciphertext, newcleartext;
+
+    cleartext = CLEARTEXT;
+    printchar(cleartext, "cleartext");
+
+    key = KEY;
+    printchar(key, "key");
+
+    ciphertext = cleartext ^ key;
+
+    printchar(ciphertext, "ciphertext");
+
+    newcleartext = ciphertext ^ key;
+    printchar(newcleartext, "newcleartext");
+
+    return 0;
+}
